c2b0e8ced1c113d52347c5908c1ea15f93a4dd3c
wolanyo <wolasoftcorporation@gmail.com>
2015-03-21 11:43:35 +0100

    First Commit Project bases No classe or code Empty
    First on my experimental branch

---
+++ core/src/main/java/fr/utbm/lo54/coursesmanager/core/App.java
@@ -0,0 +1,13 @@
+package fr.utbm.lo54.coursesmanager.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
