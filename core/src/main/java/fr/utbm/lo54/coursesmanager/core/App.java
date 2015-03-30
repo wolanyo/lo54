bb85a254ff28686dafd7cbd6a585081eb3a1c314
wolanyo <Kemour>
2015-03-30 18:29:59 +0200

    New Project

--- core/src/main/java/fr/utbm/lo54/coursesmanager/core/App.java
+++ core/src/main/java/fr/utbm/lo54/coursesmanager/core/App.java
@@ -1,12 +1,7 @@
 package fr.utbm.lo54.coursesmanager.core;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
+public class App {
+	public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
